cf459a5edf6fecc28e59030af3df9b0441cc2cb7
sooyoung <sooyoungkwak9808@gmail.com>
2025-07-22 10:49:54 +0900

    ignore set

--- src/main/java/com/sy/sncoffee/mapper/SampleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sy.sncoffee.mapper;
-
-import org.springframework.web.bind.annotation.Mapping;
-
-@Mapping
-public interface SampleMapper {
-}
--- src/main/java/com/sy/sncoffee/service/SampleService.java
+++
@@ -1,7 +0,0 @@
-package com.sy.sncoffee.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SampleService {
-}
